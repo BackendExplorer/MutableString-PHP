862283804e43a9643a8088f9c44f41dfbcb2ee1d
BackendExplorer <146496991+BackendExplorer@users.noreply.github.com>
2025-04-13 08:54:47 +0900

    Update mutableString.php

--- mutableString.php
+++ mutableString.php
@@ -2,7 +2,7 @@
 
 class MutableString {
     
-    private array $chars;
+    private array $chars = [];
 
     public function __construct(string $initialString = ""): void {
         $this->chars = mb_str_split($initialString);
